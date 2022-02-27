e05317cfeb5ee2ce244dae3771123815dc9328f5
Pin Lin <pinlin945@gmail.com>
2022-02-27 23:03:31 +0800

    Add shuffle command

---
+++ queue_shuffle.h
@@ -0,0 +1,11 @@
+#ifndef LAB0_QUEUE_SHUFFLE_H
+#define LAB0_QUEUE_SHUFFLE_H
+
+#include "list.h"
+
+/*
+ * Attempt to shuffle every node.
+ */
+void q_shuffle(struct list_head *head);
+
+#endif /* LAB0_QUEUE_EXT_H */
